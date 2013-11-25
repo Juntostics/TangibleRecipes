d6d7e34b4d76650156e4dc83b84105069acbb2f7
daisy1754 <d-part@hotmail.co.jp>
2013-11-25 21:42:30 +0900

    Add IBOutlet of label for food name

--- TangibleRecipe/Classes/TRFoodPhotosViewController.h
+++ TangibleRecipe/Classes/TRFoodPhotosViewController.h
@@ -8,6 +8,7 @@
 @interface TRFoodPhotosViewController : UIViewController
 
 @property (weak, nonatomic) IBOutlet UIImageView *foodImageView;
+@property (weak, nonatomic) IBOutlet UILabel *foodName;
 
 - (IBAction)transitToRecipes:(id)sender;
 @end
